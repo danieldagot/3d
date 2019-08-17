6ce629afe604f467bb8f7cb2c43df99076164030
danieldagot <51596364+danieldagot@users.noreply.github.com>
2019-08-17 21:04:14 +0300

    Add files via upload

---
+++ precache-manifest.10fdf3d0b6825e29c12a94653a7a178e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fcd844388ce1421d35cf1d43eb31ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "203087d4802e460c0dc4",
+    "url": "/static/css/main.c725173a.chunk.css"
+  },
+  {
+    "revision": "29fc7aaf7c52df910c06",
+    "url": "/static/js/2.cbfc1623.chunk.js"
+  },
+  {
+    "revision": "203087d4802e460c0dc4",
+    "url": "/static/js/main.eeb0a1b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
